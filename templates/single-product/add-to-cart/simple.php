19db387f3457222524dadfe7c16ca10eb52f1906
masoninthesis <mason.lawlor@me.com>
2017-01-14 20:36:22 -0700

    Changed 'add to cart' to 'add to bag'

--- templates/single-product/add-to-cart/simple.php
+++ templates/single-product/add-to-cart/simple.php
@@ -47,7 +47,7 @@ if ( $product->is_in_stock() ) : ?>
 
 	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" />
 
-	 	<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+	 	<button type="submit" class="single_add_to_cart_button button alt">Add to Bag</button>
 
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
